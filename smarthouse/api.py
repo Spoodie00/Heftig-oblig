3cfe8dda68f4977de1b8ba7ec6aba88859cbb0e0
Spoodie00 <kM@ctB53bybE9Di>
2024-04-03 20:45:55 +0200

    Oblig C

---
+++ smarthouse/api.py
@@ -0,0 +1,61 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
+from smarthouse.persistence import SmartHouseRepository
+from pathlib import Path
+import os
+def setup_database():
+    project_dir = Path(__file__).parent.parent
+    db_file = project_dir / "data" / "db.sql" # you have to adjust this if you have changed the file name of the database
+    return SmartHouseRepository(str(db_file.absolute()))
+
+app = FastAPI()
+
+repo = setup_database()
+
+smarthouse = repo.load_smarthouse_deep()
+
+if not (Path.cwd() / "www").exists():
+    os.chdir(Path.cwd().parent)
+if (Path.cwd() / "www").exists():
+    # http://localhost:8000/welcome/index.html
+    app.mount("/static", StaticFiles(directory="www"), name="static")
+
+
+# http://localhost:8000/ -> welcome page
+@app.get("/")
+def root():
+    return RedirectResponse("/static/index.html")
+
+
+# Health Check / Hello World
+@app.get("/hello")
+def hello(name: str = "world"):
+    return {"hello": name}
+
+
+# Starting point ...
+
+@app.get("/smarthouse")
+def get_smarthouse_info() -> dict[str, int | float]:
+    """
+    This endpoint returns an object that provides information
+    about the general structure of the smarthouse.
+    """
+    return {
+        "no_rooms": len(smarthouse.get_rooms()),
+        "no_floors": len(smarthouse.get_floors()),
+        "registered_devices": len(smarthouse.get_devices()),
+        "area": smarthouse.get_area()
+    }
+
+# TODO: implement the remaining HTTP endpoints as requested in
+# https://github.com/selabhvl/ing301-projectpartC-startcode?tab=readme-ov-file#oppgavebeskrivelse
+# here ...
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
