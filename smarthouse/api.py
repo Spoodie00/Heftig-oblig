2860482173605267b248b890454daac6b6dff9dd
Spoodie00 <kM@ctB53bybE9Di>
2024-04-07 17:00:37 +0200

    fikset

--- smarthouse/api.py
+++ smarthouse/api.py
@@ -24,6 +24,30 @@ if (Path.cwd() / "www").exists():
     app.mount("/static", StaticFiles(directory="www"), name="static")
 
 
+def lookup(table, list_of_columns, fetch="all", condition_variable="", condition_value=""):
+
+    if fetch not in ["all", "one"]:
+        raise ValueError("fetch parameter must be 'all' or 'one'")
+
+    connector = repo.conn.cursor()
+    columns = ", ".join(list_of_columns)
+    query = f"""
+        SELECT {columns}
+        FROM {table}
+    """
+
+    if condition_variable and condition_value:
+        query += f"WHERE {condition_variable} = {condition_value}"
+
+    if fetch == "all":
+        output = connector.execute(query).fetchall()
+    else:
+        output = connector.execute(query).fetchone()
+
+    connector.close()
+    return output
+
+
 # http://localhost:8000/ -> welcome page
 @app.get("/")
 def root():
@@ -52,105 +76,81 @@ def get_smarthouse_info() -> dict[str, int | float]:
     }
 
 
-@app.get("/smarthouse/floor", response_class=HTMLResponse)
+@app.get("/smarthouse/floor")
 def get_floor_info():
-    connector = repo.conn.cursor()
-    reading = connector.execute("""
-        SELECT floor
-        FROM rooms
-        """, ).fetchall()
-    connector.close()
+    reading = lookup("rooms", ["floor"])
 
-    output = []
+    output = {"floors": []}
 
-    for line in reading:
-        if line[0] not in output:
-            output.append(line[0])
+    for floor in reading:
+        if floor[0] not in output["floors"]:
+            output["floors"].append(floor[0])
 
     return output
 
 
-@app.get("/smarthouse/floor/{fid}", response_class=HTMLResponse)
+@app.get("/smarthouse/floor/{fid}")
 def get_floor_info(fid):
-    connector = repo.conn.cursor()
-    reading = connector.execute("""
-        SELECT area, name
-        FROM rooms
-        WHERE floor = ?
-        """, (fid,)).fetchall()
-    connector.close()
+
+    reading = lookup("rooms", ["area", "name"], condition_variable="floor", condition_value=fid)
+
     sqm = 0
     rooms = []
     for line in reading:
         sqm += line[0]
         rooms.append(line[1])
 
-    output = "Square meters: {} <br> Rooms: {}".format(sqm, rooms)
+    return {
+        "sqm": sqm,
+        "rooms": rooms,
+    }
 
-    return output
 
-
-@app.get("/smarthouse/floor/{fid}/room", response_class=HTMLResponse)
-def get_floor_info():
-    connector = repo.conn.cursor()
-    reading = connector.execute("""
-        SELECT name, id
-        FROM rooms
-        """, ).fetchall()
-    connector.close()
-    output = []
+@app.get("/smarthouse/floor/{fid}/room")
+def get_floor_rooms(fid):
+    reading = lookup("rooms", ["name", "id"], condition_variable="floor", condition_value=fid)
+    output = {}
     for line in reading:
-        output.append(list((line[1], line[0])))
+        output[line[0]] = line[1]
 
     return output
 
 
-@app.get("/smarthouse/floor/{fid}/room/{rid}", response_class=HTMLResponse)
-def get_floor_info(rid):
-    connector = repo.conn.cursor()
-    reading = connector.execute("""
-        SELECT name, area
-        FROM rooms
-        WHERE id = ?
-        """, (rid,)).fetchall()
+@app.get("/smarthouse/floor/{fid}/room/{rid}")
+def get_specific_room(rid):
+    reading = lookup("rooms", ["name", "area"], fetch="one", condition_variable="id", condition_value=rid)
+    devices = lookup("devices", ["id", "supplier", "product"], condition_variable="room", condition_value=rid)
 
-    devices = connector.execute("""
-        SELECT id, supplier, product
-        FROM devices
-        WHERE room = ?
-        """, (rid,)).fetchall()
+    output = {
+        "name": reading[0],
+        "sqm": reading[0],
+    }
 
-    connector.close()
-
-    output = "Name: {} <br> Square meters: {}  <br> ".format(reading[0][0], reading[0][1])
-
+    linecount = 0
     for line in devices:
-        output += "<br> Device ID: {} <br> Supplier: {} <br> Name: {} <br>".format(line[0], line[1], line[2])
+        output["device " + str(linecount)] = {
+            "device id": line[0],
+            "supplier": line[1],
+            "name": line[2]
+        }
+        linecount += 1
 
     return output
 
 
-@app.get("/smarthouse/room", response_class=HTMLResponse)
-def get_floor_info():
-    connector = repo.conn.cursor()
+@app.get("/smarthouse/device")
+def get_all_devices():
+    devices = lookup("devices", ["id", "supplier", "product"])
 
-    devices = connector.execute("""
-        SELECT id, supplier, product
-        FROM devices
-        """).fetchall()
+    output = {}
 
-    connector.close()
-
-    output = ""
-
+    linecount = 0
     for line in devices:
-        output += "Device ID: {} <br> Supplier: {} <br> Name: {} <br><br>".format(line[0], line[1], line[2])
+        output["device " + str(linecount)] = {
+            "device id": line[0],
+            "supplier": line[1],
+            "name": line[2]
+        }
+        linecount += 1
 
     return output
-
-
-
-if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-
-
